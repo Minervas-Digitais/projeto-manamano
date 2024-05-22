bbce6c8c15604d7797634e2f9d506415ce8c94ea
rayane <rayane.domingos288@gmail.com>
2024-05-21 23:21:20 -0300

    :wrench: eslint e prettier

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,28 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  extends: [
+    'expo',
+    'prettier',
+    'airbnb',
+    'airbnb-typescript',
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    'linebreak-style': 'off',
+    "prettier/prettier": [
+      "error",
+      {
+        "endOfLine": "auto"
+      },
+    ],
+    '@typescript-eslint/no-use-before-define': 'off',
+    'react/style-prop-object': 'off'
+  }, 
+  plugins: ['prettier'],
+}
+
