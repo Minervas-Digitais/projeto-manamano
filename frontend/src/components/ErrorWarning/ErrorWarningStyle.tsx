d480811f3a347f636a227b8ecd7afe5d8bc6b498
rayane <rayane.domingos288@gmail.com>
2024-06-05 11:31:46 -0300

    :sparkles: tela cadastro finalizada

---
+++ frontend/src/components/ErrorWarning/ErrorWarningStyle.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable import/prefer-default-export */
+import styled from 'styled-components/native';
+
+export const ErrorWarningContainer = styled.View`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  width: 100%;
+`;
+
+export const ErrorWarningText = styled.Text`
+  color: red;
+`;
