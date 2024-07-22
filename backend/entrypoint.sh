91409f4e58e9bcbd81ec9e24c445bfdc27d6b7b1
Pedro Mateus <pedromateus18@hotmail.com>
2024-07-22 11:32:43 -0300

    :zap: adding live reload docker

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh  
-npx prisma migrate deploy --schema=/usr/src/app/prisma/schema.prisma  
-npx prisma generate --schema=/usr/src/app/prisma/schema.prisma  
-npx prisma migrate dev --name init --schema=/usr/src/app/prisma/schema.prisma
+npx prisma migrate deploy --schema=/app/prisma/schema.prisma  
+npx prisma generate --schema=/app/prisma/schema.prisma  
+npx prisma migrate dev --name init --schema=/app/prisma/schema.prisma
 exec "$@"
