d9cacc022abc1d0300b9a84b80b415416e82f0cd
NicolasDSB <nicolassb@dcc.ufrj.br>
2024-07-27 00:16:29 -0300

    :sparkles: tela mudar senha feita

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,20 +1,18 @@
 /* eslint-disable global-require */
-import { StyleSheet, View } from 'react-native';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Config from './src/pages/Configuration/Configuration';
-import ConfigNotification from './src/pages/ConfigNotification/ConfigNotification';
+import ChangePassword from './src/pages/ChangePassword/ChangePassword';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer independent>
-      <Stack.Navigator initialRouteName="Notificações">
+      <Stack.Navigator initialRouteName="MudarSenha">
         <Stack.Screen
-          name="Notificações"
-          component={ConfigNotification}
+          name="MudarSenha"
+          component={ChangePassword}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
