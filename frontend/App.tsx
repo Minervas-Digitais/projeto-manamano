8a8129471404e36e7481537120da35a72462b746
NicolasDSB <nicolassb@dcc.ufrj.br>
2024-07-29 18:33:30 -0300

    :sparkles: tela entrar grupo finalizada

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -2,19 +2,15 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import ChangePassword from './src/pages/ChangePassword/ChangePassword';
+import EnterGroup from './src/pages/EnterGroup/EnterGroup';
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer independent>
-      <Stack.Navigator initialRouteName="MudarSenha">
-        <Stack.Screen
-          name="MudarSenha"
-          component={ChangePassword}
-          options={{ headerShown: false }}
-        />
+      <Stack.Navigator initialRouteName="EntrarGrupo">
+        <Stack.Screen name="EntrarGrupo" component={EnterGroup} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
