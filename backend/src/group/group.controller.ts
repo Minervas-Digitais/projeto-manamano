86cf4414c0d990795be77fafecae6c11b2fa7ddf
Pedro Mateus <pedromateus18@hotmail.com>
2024-08-02 11:55:44 -0300

    :sparkles: auth nos endpoints

--- backend/src/group/group.controller.ts
+++ backend/src/group/group.controller.ts
@@ -7,7 +7,9 @@ import {
   Param,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { CreateGroupDto } from './dto/create-group.dto';
 import { UpdateGroupDto } from './dto/update-group.dto';
 import { GroupService } from './group.service';
@@ -18,30 +20,35 @@ export class GroupController {
 
   @HttpCode(201)
   @Post()
+  @UseGuards(JwtAuthGuard)
   create(@Body() createGroupDto: CreateGroupDto) {
     return this.groupService.create(createGroupDto);
   }
 
   @HttpCode(200)
   @Get()
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.groupService.findAll();
   }
 
   @HttpCode(200)
   @Get(':id')
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.groupService.findOne(id);
   }
 
   @HttpCode(201)
   @Patch(':id')
+  @UseGuards(JwtAuthGuard)
   update(@Param('id') id: string, @Body() updateGroupDto: UpdateGroupDto) {
     return this.groupService.update(id, updateGroupDto);
   }
 
   @HttpCode(204)
   @Delete(':id')
+  @UseGuards(JwtAuthGuard)
   remove(@Param('id') id: string) {
     return this.groupService.remove(id);
   }
