fbb512bd44c689213a65483e83e954c3f82aa9fb
NicolasDSB <nicolassb@dcc.ufrj.br>
2025-02-08 08:56:13 -0300

    :sparkles: conexão com backend

--- frontend/src/pages/ChangePassword/ChangePassword.tsx
+++ frontend/src/pages/ChangePassword/ChangePassword.tsx
@@ -2,23 +2,61 @@
 import { useFonts } from 'expo-font';
 import { View } from 'react-native';
 import { Controller, useForm } from 'react-hook-form';
+import { useEffect, useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import HeaderCustom from '../../components/HeaderCustom/HeaderCustom';
 import InputTextCustom from '../../components/InputText/InputTextCustom';
 import ErrorWarning from '../../components/ErrorWarning/ErrorWarning';
 import { SignInForm, SignInInputContainer } from '../SignIn/SignInStyle';
 import ButtonCustom from '../../components/ButtonCustom/ButtonCustom';
+import { storage } from '../SignIn/SignIn';
+import api from '../../services/api';
 
 export default function ChangePassword() {
+  const navigation = useNavigation();
   const iconPassword = require('../../assets/lock-icon.svg');
+  const [loggedIdState, setLoggedIdState] = useState('');
+  const [accessTokenState, setAccessTokenState] = useState('');
+  useEffect(() => {
+    const accessToken = storage.getString('accessToken');
+    const loggedId = storage.getString('loggedId');
+    if (loggedId && accessToken) {
+      setAccessTokenState(accessToken);
+      setLoggedIdState(loggedId);
+      api.get(`/user/${loggedId}`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+    }
+  }, []);
   const {
     control,
     handleSubmit,
     formState: { errors },
     getValues,
   } = useForm({});
-  const onSubmit = (data: any) => {
-    // eslint-disable-next-line no-alert
-    alert(JSON.stringify(data));
+  const onSubmit = async (data: any) => {
+    try {
+      const response = await api.patch(
+        `/user/${loggedIdState}/change-password`,
+        {
+          id: loggedIdState,
+          oldPassword: data.oldpassword,
+          newPassword: data.newpassword,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${accessTokenState}`,
+          },
+        },
+      );
+      alert('Senha atualizada com sucesso!');
+      navigation.navigate('Configurações');
+    } catch (error) {
+      console.error('Erro ao mudar senha:', error);
+      alert('Erro ao mudar senha. Tente novamente mais tarde.');
+    }
   };
   const [fontsLoaded] = useFonts({
     'inter-bold': require('../../fonts/Inter-Bold.ttf'),
@@ -27,7 +65,12 @@ export default function ChangePassword() {
     return undefined;
   }
   return (
-    <View style={{ flex: 1, backgroundColor: '#f2f6fa' }}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: '#f2f6fa',
+        display: loggedIdState && accessTokenState ? 'flex' : 'none',
+      }}>
       <HeaderCustom font="inter-bold" text="Mudar Senha" />
       <SignInForm>
         <SignInInputContainer>
