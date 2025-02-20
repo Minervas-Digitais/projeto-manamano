f23d5fe639fb7d410679efccf3968aa8b19480a1
tales <talescmoreira@gmail.com>
2025-02-20 13:24:58 -0300

    :bug: alteração da controller e service para aderir ao DTO

--- backend/src/archive/archive.module.ts
+++ backend/src/archive/archive.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ArchiveService } from './archive.service'; 
-import { ArchiveController } from './archive.controller'; 
-import { PrismaService } from '../prisma/prisma.service'; 
+import { ArchiveService } from './archive.service';
+import { ArchiveController } from './archive.controller';
+import { PrismaService } from '../prisma/prisma.service';
+
 @Module({
-  controllers: [ArchiveController], 
+  controllers: [ArchiveController],
   providers: [ArchiveService, PrismaService],
 })
 export class ArchiveModule {}
--- backend/src/archive/archive.service.ts
+++ backend/src/archive/archive.service.ts
@@ -1,31 +1,27 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
-import { ArchiveEntity } from './entity/archive.entity';
+import { CreateArchiveDto, ResponseArchiveDto } from './dto/archive.dto';
 
 @Injectable()
 export class ArchiveService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async createArchive(data: {
-    name: string;
-    mimeType: string;
-    contentBase64: string;
-    userId?: string | null;
-    groupId?: string | null;
-  }): Promise<ArchiveEntity> {
+  async createArchive(data: CreateArchiveDto): Promise<ResponseArchiveDto> {
     const createdArchive = await this.prisma.archive.create({
       data: {
         name: data.name,
         mimeType: data.mimeType,
         contentBase64: data.contentBase64,
-        userId: data.userId || null,
-        groupId: data.groupId || null,
+        userId: data.userId ? String(data.userId) : null,  // Convertendo número para string
+        groupId: data.groupId ? String(data.groupId) : null, // Convertendo número para string
+
       },
     });
-    return new ArchiveEntity(createdArchive);
+
+    return this.mapToResponseDto(createdArchive);
   }
 
-  async getArchiveById(id: string): Promise<ArchiveEntity> {
+  async getArchiveById(id: string): Promise<ResponseArchiveDto> {
     const archive = await this.prisma.archive.findUnique({
       where: { id },
     });
@@ -34,6 +30,19 @@ export class ArchiveService {
       throw new NotFoundException('Archive not found');
     }
 
-    return new ArchiveEntity(archive);
+    return this.mapToResponseDto(archive);
+  }
+
+  private mapToResponseDto(archive: any): ResponseArchiveDto {
+    return {
+      id: archive.id,
+      name: archive.name,
+      mimeType: archive.mimeType,
+      type: archive.type,
+      userId: archive.userId ? Number(archive.userId) : undefined, // Convertendo string para número
+      groupId: archive.groupId ? Number(archive.groupId) : undefined, // Convertendo string para número
+
+    };
   }
 }
+
