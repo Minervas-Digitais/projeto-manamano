b7c50bc090adeabe07723c062a3c2dae59e8ea35
kyystt <gui.sobreira10@gmail.com>
2025-05-15 21:12:01 -0300

    Testes funcionais para notification feitos

--- backend/src/notification/tests/notification.e2e-spec.ts
+++ backend/src/notification/tests/notification.e2e-spec.ts
@@ -4,10 +4,11 @@ import { PrismaService } from "src/prisma/prisma.service";
 import { NotificationModule } from "../notification.module";
 import { UserModule } from "src/user/user.module";
 import { AuthModule } from "src/auth/auth.module";
-import { getUserToken, resetDatabase } from "../../../test/test-helper.notification";
+import { getAdminToken, getNotificationId, getRecipientToken, getSenderToken, getUserToken, resetDatabase } from "../../../test/test-helper.notification";
 import { CreateNotificationDto } from "../dto/create-notification.dto";
 import { NotificationType } from "@prisma/client";
 import request from "supertest";
+import { UpdateNotificationDto } from "../dto/update-notification.dto";
 
 describe("Notification", () => {
     let app: INestApplication;
@@ -32,10 +33,21 @@ describe("Notification", () => {
 
     describe("Create", () => {
         it("Deve criar uma notificacao", async () => {
+            // deve criar um sender e um recipient para criar notification
+            const recipientToken = await getRecipientToken(app, prismaService)
+            const recipientUser = await prismaService.user.findUnique({
+                where: {email: "testrecipient@example.com"}
+            })
+
+            const userToken = await getUserToken(app, prismaService);
+            const senderUser = await prismaService.user.findUnique({
+                where: {email: "testuser@example.com"},
+            });
+
             const notificationDTO: CreateNotificationDto = {
-                senderId: "1",
+                senderId: senderUser.id,
                 body: "bodyTeste",
-                recipientId: "1",
+                recipientId: recipientUser.id,
                 type: NotificationType.COMMENT,
                 groupName: "groupTeste",
                 senderName: "senderTeste"
@@ -53,7 +65,6 @@ describe("Notification", () => {
             expect(response.body).toHaveProperty('recipientId');
             expect(response.body).toHaveProperty('type');
             expect(response.body).toHaveProperty('createdAt');
-            expect(response.body).toHaveProperty('updatedAt');
         })
 
         it("deve retornar erro 400 caso os campos forem invalidos", async () => {
@@ -78,37 +89,202 @@ describe("Notification", () => {
         })
     })
 
+    describe("CreateGlobal", () => {
+        it("Deve criar uma notificacao global apenas se for admin", async () => {
+            const recipientToken = await getRecipientToken(app, prismaService)
+            const recipientUser = await prismaService.user.findUnique({
+                where: {email: "testrecipient@example.com"}
+            })
+
+            const adminToken = await getAdminToken(app, prismaService);
+            const adminUser = await prismaService.user.findUnique({
+                where: {email: "admin@example.com"},
+            });
+
+            const notificationDTO = {
+                body: "bodyTeste",
+                recipientId: recipientUser.id,
+                type: NotificationType.COMMENT,
+                groupName: "groupTeste",
+                senderName: "senderTeste"
+            }
+
+            const response = await request(app.getHttpServer())
+                .post("/notifications/global")
+                .set("Authorization", "Bearer " + adminToken)
+                .send(notificationDTO)
+            
+            expect(response.status).toBe(201)
+            expect(response.body).toHaveProperty('id');
+            expect(response.body).toHaveProperty('senderId');
+            expect(response.body).toHaveProperty('body');
+            expect(response.body).toHaveProperty('recipientId');
+            expect(response.body).toHaveProperty('type');
+            expect(response.body).toHaveProperty('createdAt');
+        })
+
+        it("Deve negar acesso a token de usuario", async () => {
+            const recipientToken = await getRecipientToken(app, prismaService)
+            const recipientUser = await prismaService.user.findUnique({
+                where: {email: "testrecipient@example.com"}
+            })
+
+            const userToken = await getUserToken(app, prismaService);
+            const user = await prismaService.user.findUnique({
+                where: {email: "testuser@example.com"},
+            });
+
+            const notificationDTO: CreateNotificationDto = {
+                senderId: user.id,
+                body: "bodyTeste",
+                recipientId: recipientUser.id,
+                type: NotificationType.COMMENT,
+                groupName: "groupTeste",
+                senderName: "senderTeste"
+            }
+
+            const response = await request(app.getHttpServer())
+                .post("/notifications/global")
+                .set("Authorization", "Bearer " + userToken)
+                .send(notificationDTO)
+
+            expect(response.status).toBe(403)
+            expect(response.body.message).toBe("Forbidden resource")
+        })
+    })
+
     describe("getNotificationsForUser", () => {
         it("Deve retornar todas as notificacoes de um usuario", async () => {
             // Criar usuario 
+            const sendingToken = await getSenderToken(app, prismaService)
+            const sendingUser = await prismaService.user.findUnique({
+                where: {email: "testsender@example.com"}
+            })
+
+            const recipientToken = await getRecipientToken(app, prismaService);
+            const recipientUser = await prismaService.user.findUnique({
+                where: {email: "testrecipient@example.com"},
+            });
+            
             // Criar uma notificacao no id do usuario
+            const notificationDTO: CreateNotificationDto = {
+                senderId: sendingUser.id,
+                body: "bodyTeste",
+                recipientId: recipientUser.id,
+                type: NotificationType.COMMENT,
+                groupName: "groupTeste",
+                senderName: "senderTeste"
+            }
+
+            const notificationSent = await prismaService.notification.createMany({
+                data: [{
+                    senderId: notificationDTO.senderId,
+                    recipientId: notificationDTO.recipientId,
+                    body: notificationDTO.body,
+                    type: notificationDTO.type,
+                    groupName: notificationDTO.groupName || null,
+                    senderName: notificationDTO.senderName || null,
+                }, 
+                {
+                    senderId: notificationDTO.senderId,
+                    recipientId: notificationDTO.recipientId,
+                    body: notificationDTO.body,
+                    type: notificationDTO.type,
+                    groupName: notificationDTO.groupName || null,
+                    senderName: notificationDTO.senderName || null,
+                }]
+            })
             // Pegar as notificacoes dele GET: user/:userId
+            
+
+            const response = await request(app.getHttpServer())
+                .get(`/notifications/user/${recipientUser.id}`)
+                .set("Authorization", "Bearer " + recipientToken)
+
+            expect(response.status).toBe(200);
+            expect(response.body[0].senderId).toEqual(notificationDTO.senderId)
+            expect(response.body[0].recipientId).toEqual(notificationDTO.recipientId)
+            expect(response.body[1].senderId).toEqual(notificationDTO.senderId)
+            expect(response.body[1].recipientId).toEqual(notificationDTO.recipientId)
         })
 
-        it("Deve retornar erro caso o id seja invalido", async () => {
+        it("Deve retornar [] caso o id seja invalido", async () => {
             // Cria request com um id invalido
+            const recipientToken = await getUserToken(app, prismaService);
+            const id = 123 
+
+            const response = await request(app.getHttpServer())
+                .get(`/notifications/user/${id}`)
+                .set("Authorization", "Bearer " + recipientToken)
+            
+            expect(response.status).toBe(200)
+            expect(response.body).toEqual([])
         })
     })
 
     describe("markAsRead", () => {
         it("Deve marcar uma notificacao como lida", async () => {
             // Cria notificacao
+            const senderToken = await getSenderToken(app, prismaService);
+            const notificationId = await getNotificationId(app, prismaService);
+
+            const update: UpdateNotificationDto = {
+                isRead: true,
+            }
             // Marca como lida Patch: /:id
+            const response = await request(app.getHttpServer())
+                .patch(`/notifications/${notificationId}`)
+                .set("Authorization", "Bearer " + senderToken)
+                .send(update)
+            
+            expect(response.status).toBe(200)
+            expect(response.body).toHaveProperty("isRead")
+            expect(response.body.isRead).toBe(true)
         })
 
         it("Deve retornar erro caso o id seja invalido", async () => {
             // Cria request com um id invalido
+            const senderToken = await getSenderToken(app, prismaService);
+            const notificationId = 123
+            const update: UpdateNotificationDto = {
+                isRead: true,
+            }
+
+            const response = await request(app.getHttpServer())
+                .patch(`/notifications/${notificationId}`)
+                .set("Authorization", "Bearer " + senderToken)
+                .send(update)
+            
+            expect(response.status).toBe(500)
+            expect(response.body.message).toBe("Internal server error")
         })
     })
 
     describe("deleteNotification", () => {
         it("Deve deletar uma notificacao", async () => {
             // Cria notificacao
+            const senderToken = await getSenderToken(app, prismaService);
+            const notificationId = await getNotificationId(app, prismaService);
             // Deleta notificacao Delete: /:id
+            const response = await request(app.getHttpServer())
+                .delete(`/notifications/${notificationId}`)
+                .set("Authorization", "Bearer " + senderToken)
+            
+            expect(response.status).toBe(200)
+            expect(response.body).toHaveProperty("id")
         })
         
         it("Deve retornar erro caso o id seja invalido", async () => {
             // Cria request com um id invalido
+            const senderToken = await getSenderToken(app, prismaService);
+            const notificationId = 123
+
+            const response = await request(app.getHttpServer())
+                .delete(`/notifications/${notificationId}`)
+                .set("Authorization", "Bearer " + senderToken)
+            
+            expect(response.status).toBe(500)
+            expect(response.body.message).toBe("Internal server error")
         })
     })
 })
--- backend/test/test-helper.notification.ts
+++ backend/test/test-helper.notification.ts
@@ -3,7 +3,37 @@ import * as bcrypt from 'bcrypt';
 import request from "supertest";
 import { PrismaService } from "src/prisma/prisma.service";
 import { execSync } from "child_process";
-import { RoleType } from "@prisma/client";
+import { NotificationType, RoleType } from "@prisma/client";
+import { CreateNotificationDto } from "src/notification/dto/create-notification.dto";
+
+export async function getNotificationId(app: INestApplication, prisma: PrismaService) {
+    const recipientToken = await getRecipientToken(app, prisma)
+    const recipientUser = await prisma.user.findUnique({
+            where: {email: "testrecipient@example.com"}
+        })
+
+    const senderToken = await getSenderToken(app, prisma);
+    const senderUser = await prisma.user.findUnique({
+            where: {email: "testuser@example.com"},
+        })
+    
+    const notificationDTO: CreateNotificationDto = {
+            senderId: senderUser.id,
+            body: "bodyTeste",
+            recipientId: recipientUser.id,
+            type: NotificationType.COMMENT,
+            groupName: "groupTeste",
+            senderName: "senderTeste"
+        }
+        
+    const response = await request(app.getHttpServer())
+            .post("/notifications")
+            .set("Authorization", "Bearer " + senderToken)
+            .send(notificationDTO)
+            .expect(201)
+    
+    return response.body.id
+}
 
 export async function getUserToken(app: INestApplication, prisma: PrismaService) {
     const email = 'testuser@example.com';
@@ -34,6 +64,67 @@ export async function getUserToken(app: INestApplication, prisma: PrismaService)
 
     return loginResponse.body.accessToken;
 }
+
+export async function getRecipientToken(app: INestApplication, prisma: PrismaService) {
+    const email = 'testrecipient@example.com';
+
+    const existingUser = await prisma.user.findUnique({
+        where: { email },
+    });
+
+    if (!existingUser) {
+        await request(app.getHttpServer())
+            .post('/user')
+            .send({
+                fullName: 'TestRecipientUser',
+                email,
+                phone: '1234567891',
+                hash: 'password123',
+            })
+            .expect(201);
+    }
+
+    const loginResponse = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({
+            email,
+            password: 'password123',
+        })
+        .expect(201);
+
+    return loginResponse.body.accessToken;
+}
+
+export async function getSenderToken(app: INestApplication, prisma: PrismaService) {
+    const email = 'testsender@example.com';
+
+    const existingUser = await prisma.user.findUnique({
+        where: { email },
+    });
+
+    if (!existingUser) {
+        await request(app.getHttpServer())
+            .post('/user')
+            .send({
+                fullName: 'TestSenderUser',
+                email,
+                phone: '1234567892',
+                hash: 'password123',
+            })
+            .expect(201);
+    }
+
+    const loginResponse = await request(app.getHttpServer())
+        .post('/auth/login')
+        .send({
+            email,
+            password: 'password123',
+        })
+        .expect(201);
+
+    return loginResponse.body.accessToken;
+}
+
 
 export function resetDatabase() {
     try {
