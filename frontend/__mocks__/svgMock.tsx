3d3bd3d279a79b6e8ca83d921f91dbea362b6d47
kyystt <gui.sobreira10@gmail.com>
2025-06-13 20:36:14 -0300

    Comentarios para compreensao dos arquivos de mock

--- frontend/__mocks__/svgMock.tsx
+++ frontend/__mocks__/svgMock.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 
+// Semelhantemente ao fileMock.ts, mas especificamente para SVGs 
+// que o jest precisa que seja mockado para realizar os testes
+
 const SvgMock = React.forwardRef<
   HTMLSpanElement, 
   React.ComponentProps<'span'>
