c56c420d375c0ba878b81c74efcfd20bc8e5d9e9
jho-on <jhonatasousa288@gmail.com>
2026-04-20 14:40:47 -0300

    Migração para npm e adição do env no front

--- frontend/src/services/api.tsx
+++ frontend/src/services/api.tsx
@@ -1,8 +1,7 @@
 import axios from 'axios';
-import { Platform } from 'react-native';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.7:3000',
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   validateStatus: (status) => status < 400,
 });
 export default api;
