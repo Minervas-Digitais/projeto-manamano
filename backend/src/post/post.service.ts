81d6e052e86e648d95a511471cae7a661239b856
jho---on <jhonatasousa288@gmail.com>
2026-09-22 23:10:55 -0300

    fix: prettier post.service imports pós-rebase

--- backend/src/post/post.service.ts
+++ backend/src/post/post.service.ts
@@ -1,4 +1,9 @@
-import { BadRequestException, ForbiddenException, Injectable, NotFoundException } from '@nestjs/common';
+import {
+  BadRequestException,
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
